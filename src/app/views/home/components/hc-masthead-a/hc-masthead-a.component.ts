030c588d4d77e00e20480225afd8da2b9702c9bf
JavierTorijanoGaroz <jtorijanogaroz@gmail.com>
2019-04-28 18:34:09 +0200

    Feature HU.WEB08
    Home components: "Patients Search", "Masthead-A".

---
+++ src/app/views/home/components/hc-masthead-a/hc-masthead-a.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hc-masthead-a',
+  templateUrl: './hc-masthead-a.component.html',
+  styleUrls: ['./hc-masthead-a.component.scss']
+})
+export class HcMastheadAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/views/home/components/hc-patients-search/hc-patients-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hc-patients-search',
+  templateUrl: './hc-patients-search.component.html',
+  styleUrls: ['./hc-patients-search.component.scss']
+})
+export class HcPatientsSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
