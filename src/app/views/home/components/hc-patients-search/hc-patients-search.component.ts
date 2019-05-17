6e8d712355fd8ed8bb8b1de067102e1403365fc8
JavierTorijanoGaroz <jtorijanogaroz@gmail.com>
2019-05-17 19:38:10 +0200

    Feature HU.MGMTPATIENT04 Add Patient search component.

--- src/app/views/home/components/hc-patients-search/hc-patients-search.component.ts
+++ src/app/views/home/components/hc-patients-search/hc-patients-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PatientsService } from 'src/app/core/patients.service';
+import { Patient } from '../../../../core/patient.model';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-hc-patients-search',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HcPatientsSearchComponent implements OnInit {
 
-  constructor() { }
+  patient: Patient
+  flag: boolean
+
+  constructor(private ps: PatientsService) { }
 
   ngOnInit() {
   }
 
+  onReadPatient(form: NgForm): void {
+    this.ps.getPatientByCIP(form.value.searchField).subscribe(patients => {
+      this.patient = patients[0]
+      this.flag = (patients) ? true : false
+    }) 
+  }
 }
